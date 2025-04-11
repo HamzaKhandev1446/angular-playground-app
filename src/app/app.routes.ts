b01203535c7e009e90cf67a70f277c880a0e0d46
hamza khan <hamzakhan.dev1446@gmail.com>
2025-04-11 23:26:44 +0500

    Added routing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'playground',
+    loadComponent: () =>
+      import('./playground/playground.component').then(m => m.PlaygroundComponent),
+    children: [
+      {
+        path: 'frontward-solutions',
+        loadComponent: () =>
+          import('./frontward-solution/frontward-solution.component').then(m => m.FrontwardSolutionComponent),
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'frontward-solutions'   // Optional: Default child route
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
---
+++ src/app/frontward-solution/frontward-solution.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-frontward-solution',
+  imports: [],
+  templateUrl: './frontward-solution.component.html',
+  styleUrl: './frontward-solution.component.scss'
+})
+export class FrontwardSolutionComponent {
+
+}
